6d0213a2d9c079678e02c51ef7f75c69c6d6fb28
ray <raylimb15@gmail.com>
2026-04-04 22:13:37 -0400

    #todo: consoleidate boxscore api calls to one call

--- fudge.py
+++ fudge.py
@@ -1,16 +1,19 @@
 import discord
 import aiohttp
 import asyncio
+import os
+from dotenv import load_dotenv
 from datetime import date
 
 import ssl
 import certifi
 
+load_dotenv()
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 # ---- CONFIG ----
-DISCORD_TOKEN = 
-CHANNEL_ID = 
+DISCORD_TOKEN = os.getenv("discord_token")
+CHANNEL_ID = int(os.getenv("channel_id"))
 CHECK_INTERVAL = 30  # seconds between polls
 JUDGE_ID = 592450  # Aaron Judge's MLB player ID: 592450
 # ----------------
@@ -20,6 +23,8 @@ connector = aiohttp.TCPConnector(ssl=ssl_context)
 client = discord.Client(intents=intents, connector=connector)
 
 known_strikeout_count = None
+airout = None
+groundout = None
 
 async def get_todays_game_pks():
     today = date.today().strftime("%Y-%m-%d")
@@ -46,9 +51,25 @@ async def get_judge_strikeouts(game_pk):
             stats = players[key].get("stats", {}).get("batting", {})
             return stats.get("strikeOuts", 0)
     return None
+#combine get_judge_strikeouts and outty into one function to reduce redundant API calls
+async def outty(game_pk):
+    url = f"https://statsapi.mlb.com/api/v1/game/{game_pk}/boxscore"
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+        async with session.get(url) as r:
+            data = await r.json()
+
+    for side in ["away", "home"]:
+        players = data.get("teams", {}).get(side, {}).get("players", {})
+        key = f"ID{JUDGE_ID}"
+        if key in players:
+            stats = players[key].get("stats", {}).get("batting", {})
+            return [stats.get("airOuts", 0), stats.get("groundOuts", 0)]
+    return None
 
 async def monitor():
     global known_strikeout_count
+    global airout
+    global groundout
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
     while not client.is_closed():
@@ -57,6 +78,8 @@ async def monitor():
             print(f"[{date.today()}] calling the api... " + str(game_pks))
             for pk in game_pks:
                 strikeouts = await get_judge_strikeouts(pk)
+                outs = await outty(pk)
+                print(outs)
                 if strikeouts is None:
                     continue
                 if known_strikeout_count is None:
@@ -69,6 +92,24 @@ async def monitor():
                             f"That's strikeout #{strikeouts} today for Aaron Judge!"
                         )
                     known_strikeout_count = strikeouts
+                if airout is None:
+                    airout = outs[0]
+                elif outs[0] > airout:
+                    await channel.send(
+                            "⚾ **Aaron FUDGE JUST pop OUT!** 🙈 "
+                            f"That's popout #{outs[0]} today for Aaron Judge!"
+                        )
+                    airout = outs[0]
+
+                if groundout is None:
+                    groundout = outs[1]
+                elif outs[1] > groundout:
+                    await channel.send(
+                            "⚾ **Aaron FUDGE JUST ground OUT!** 🙈 "
+                            f"That's groundout #{outs[1]} today for Aaron Judge!"
+                        )
+                    groundout = outs[1]
+
         except Exception as e:
             print(f"Error: {e}")
 
